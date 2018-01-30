9493baab4e7a1495bc62b35f52652b797c355e24
HSNBDES33\eocariz <eocariz@hasar.com>
2018-01-30 09:24:44 -0300

    add window to avoid global variables validation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,7 +13,7 @@ const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload });
 const getWeatherCity = payload => ({type: GET_WEATHER_CITY, payload });
 const setWeatherCity = payload => ({type: SET_WEATHER_CITY, payload });
 
-const protocol = location.protocol;
+const protocol = window.location.protocol;
 
 const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
 const url_base = `${protocol}://api.openweathermap.org/data/2.5/`
